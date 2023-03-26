e7ea570db400acb0430fdd1bdc516ee8bd42743c
maxlyara1 <maks.lyara@gmail.com>
2023-03-26 20:14:53 +0300

    upload_gold

--- golden_section_search.r
+++ golden_section_search.r
@@ -1,75 +1,72 @@
-start_point = c(-8, -3) #start point
-start_step = 3 # шаг
-eps = 0.00001
-counter_needed <- 10000000000
+# Ввод
+#a = as.numeric(readline(prompt = "Введите границу А: "))
+#b = as.numeric(readline(prompt = "Введите границу B: "))
+#epsilon = as.numeric(readline(prompt = "Введите точность: "))
 
-
-
-jeevs = function(d, a, h, counter_needed){
-  counter = 0
-  e_x = c(1,0)
-  e_y = c(0,1)
-  f <- function(a) {
-    x <- a[1]
-    y <- a[2]
-    counter <<- counter + 1
-    10*sin(sqrt(x^2+y^2))*(cos(sqrt(x^2+y^2)))^2/(sqrt((x^2)+y^2))
-  }
-  graphf <- function(x, y){
-    10*sin(sqrt(x^2+y^2))*(cos(sqrt(x^2+y^2)))^2/(sqrt((x^2)+y^2))
-  }
-  #находим значения
-  graphx <- seq(-20,20,0.1)
-  graphy <- seq(-20,20,0.1) 
-  z <- outer(graphx,graphy,graphf)
-  
-  #строим графики
-  contour(graphx,graphy, z, nlevels=15) 
-  points(a[1],a[2],col='black', pch=20, cex=2)
-  
-  while(h > d & counter <= counter_needed){
-    
-    lr = which.max(c(f(a), f(a+e_x*h), f(a - e_x*h)))
-    if(lr==2){
-      a_1 = a+e_x*h
-    }
-    else if(lr==3){
-      a_1 = a - e_x*h
-    }
-    else if(lr==1){
-      a_1 = a
-    }
-    
-    ud = which.max(c(f(a_1), f(a_1+e_y*h), f(a_1 - e_y*h)))
-    if (ud == 1){
-      a_2 = a_1
-    }
-    else if(ud == 2){
-      a_2 = a_1 + e_y*h
-    }
-    else if(ud == 3){
-      a_2 = a_1 - e_y*h
-    }
-    if (all(a_2 == a)){
-      h = h/2
-    }
-    else {
-      while (f(2*a_2-a) > f(a_2)){
-        segments(a[1], a[2], a_2[1], a_2[2])
-        points(a_2[1],a_2[2],col='red',pch=20, cex=2)
-        a = a_2
-        a_2 = 2*a_2 - a
-        
-      }
-    }
-    segments(a[1], a[2], a_2[1], a_2[2])
-    points(a_2[1],a_2[2],col='green',pch=20)
-    a = a_2
-  }
-  cat("Координаты точки экстремума:", a, "\n", "Количество вычислений целевой функции:", counter)
+# Задаём нужную функцию для золотого сечения
+goldfunc = function(x){ 
+  return(sin(x))
 }
-jeevs(eps, start_point, start_step, counter_needed)
+
+# Функция вывода точек минимума
+min_point = function(a, b)
+  points((a+b)/2, goldfunc((a+b)/2))
+
+# Функция вывода крайних точек
+bounds = function(a, b){
+  points(a, goldfunc(a))
+  points(b, goldfunc(b))
+  }
+
+# С помощью золотого сечения
+gold = function(a, b, n){
+  
+  # Создаю график
+  #distx <- (a+abs(a%/%3.14)):(b-abs(b)%/%3.14)
+  distx <- seq(a, b, 0.1)
+  disty <- goldfunc(distx)
+  plot(distx, disty, type='l', xlab = "Точки", ylab = 'Значения точек',col="green",
+       main="Golden-section search")
+  
+  fi = (1 + sqrt(5)) / 2
+  x1 = b - (b - a) / fi
+  x2 = a + (b - a) / fi
+  m = c()
+    while(b - a > n){
+      # Точки границ во время цикла
+      bounds(a, b)
+      
+      # Точки минимума во время цикла
+      #min_point(a, b)
+      
+      if(goldfunc(x1) > goldfunc(x2)){
+        a = x1
+        x1 = x2
+        x2 = b - (x1 - a)
+        #m <- c(m, a)
+      }
+      else{
+        b = x2
+        x2 = x1
+        x1 = a + (b - x2)
+        #m <- c(m, b)
+      }
+      
+      # Точки границ после выполнения цикла
+      bounds(a, b)
+      
+      # Точки минимума после выполнения цикла
+      #min_point(a, b)
+    }
+  
+  # Вывод локального минимума и значения в этой точке
+  cat("Локальный минимум в точке: ", ((a+b)/2), "\n", 
+      "Значение в этой точке:", goldfunc((a+b)/2), "\n")
+      #,"Значение в этой точке(2):", max(c(goldfunc(a), goldfunc(b), goldfunc(x1), goldfunc(x1))))
+}
 
 
-#Красным отмечены точки, в которых не выполняется проверка функции во все стороны,
-#а лишь изменяется точка, в ту же сторону, в которую до этого сработала максимизация функции
+gold(-10, 10, 0.00001)
+# левая граница, правая граница, эпсилон
+
+
